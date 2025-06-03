24c5413a21be3118708be50048ae42172c50a216
Sabi Kahlon <sabikahlon7345@gmail.com>
2025-06-02 21:57:34 -0400

    Add routing and page components for Home, About, and Contact

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import "./assets/styles/styles.css"
 
-function App() {
-
+export default function App() {
   return (
-    <>
-     
-     hi
+    <> 
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
     </>
   )
-}
 
-export default App
+}
